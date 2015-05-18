b8ce6e9283532907ce1e0dda82fd9c62105cf4f6
Cedric Oltramare <cedric_mac@Cedrics-MacBook-Pro-2.local>
2015-05-17 19:54:17 -0700

    first commit

---
+++ problem1.py
@@ -0,0 +1,11 @@
+print 'hi cedric'
+
+#If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+def solve(a, b, c):
+	return a + b + c
+
+print solve(34, 23, 19)
